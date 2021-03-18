ff3add7a3fa41486a2981f85a700d96d27e29869
Jacob Friis Saxberg <jacob@webcom.dk>
2021-03-18 14:57:40 +0100

    test from terminal

--- test.js
+++ test.js
@@ -2,11 +2,19 @@ const amountToCash = require('./atm.js');
 
 const tests = [578, 100, 123, 1543];
 
-tests.forEach((value) => {
-  console.log({
-    value,
-    result: amountToCash(value),
+function test() {
+  tests.forEach((value) => {
+    console.log({
+      value,
+      result: amountToCash(value),
+    });
   });
-});
+}
 
-// console.log(amountToCash(105));
+const args = process.argv[2];
+
+if (args !== undefined) {
+  console.log(amountToCash(parseInt(args, 10)));
+} else {
+  console.log(test());
+}
