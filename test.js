87ace6d999db1331b174be4e63c4013952183666
Jacob Friis Saxberg <jacob@webcom.dk>
2021-03-22 14:51:21 +0100

    make it more functional

--- test.js
+++ test.js
@@ -2,19 +2,29 @@ const amountToCash = require('./atm.js');
 
 const tests = [578, 100, 123, 1543];
 
-function test() {
-  tests.forEach((value) => {
-    console.log({
-      value,
-      result: amountToCash(value),
-    });
-  });
+function validator(amount, result) {
+  return amount === result.reduce((acc, { value }) => acc += value, 0);
+}
+
+function test(amount) {
+  const result = amountToCash(amount);
+  const valid = validator(amount, result);
+  return { amount, result, valid };
+}
+
+function runTests() {
+  return tests.map(test);
+}
+
+function output(result) {
+  console.log(JSON.stringify(result, null, 2));
 }
 
 const args = process.argv[2];
 
 if (args !== undefined) {
-  console.log(amountToCash(parseInt(args, 10)));
+  const amount = parseInt(args, 10);
+  output(test(amount));
 } else {
-  console.log(test());
+  output(runTests());
 }
